2cb2fd61942871dc6a3553792cb4cb778dc26419
olena <lymar.olena@gmail.com>
2017-11-06 14:51:21 +0200

    carousel

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    infinite: true,
+    speed: 700
+  });
+});
