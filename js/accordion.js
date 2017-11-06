9f5dd74b4cb410328bf223ba7fe3e73a72cefced
olena <lymar.olena@gmail.com>
2017-11-06 14:50:09 +0200

    slick styles

--- js/accordion.js
+++ js/accordion.js
@@ -1,18 +1,12 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    }
-}
+$('.accordion').on('click', function() {
+	if ($(this).next().hasClass('opened')) {
+		return
+	}
+	$('.panel.opened')
+		.slideUp()
+		.removeClass('opened');
+	$(this)
+		.next()
+		.slideDown()
+		.addClass('opened');
+});
